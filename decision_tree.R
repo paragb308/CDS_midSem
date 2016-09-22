c703c1a2ce6c33c15cd604bbfc1e617c2af34fa3
paragb308 <paragb308@users.noreply.github.com>
2016-09-22 09:31:49 +0530

    Added three methods of trees:
    tree(), rpart(), and ctree()

--- decision_tree.R
+++ decision_tree.R
@@ -1,1 +1,29 @@
 
+ip_all <- c(ip1, ip2, ip3)
+formula <- as.formula(paste("op ~ ", paste(ip_all, collapse= "+")))
+
+#---------------------------------------------------------------
+require(tree)
+tree_model <- tree(formula, data = train)
+plot(tree_model)
+text(tree_model)
+iris.tree
+summary(iris.tree)
+#----------------------------------------------------------------
+library(rpart)
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+tree_model <- rpart(formula, data=train)
+fancyRpartPlot(tree_model)
+
+#----------------------------------------------------------------
+library(party)
+
+ctree_model <- tree(formula, data = train)
+plot(ctree_model)
+#----------------------------------------------------------------
+
+
+
